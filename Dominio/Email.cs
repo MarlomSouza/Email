2bfd2d4e86242415f2301519767498f48716b821
Marlom Souza <marlomsouza@digix.com.br>
2018-11-06 08:41:03 -0300

    first commit

---
+++ Dominio/Email.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dominio
+{
+    public class Email
+    {
+        public string Remetente { get; private set; }
+        public string Mensagem { get; private set; }
+        public string Destinario { get; private set; }
+
+        public Email(string remetente, string mensagem, string destinario)
+        {
+           ValidarEnderecoDeEmail(remetente);
+            ValidarEnderecoDeEmail(destinario);
+
+            Remetente = remetente;
+            Mensagem = mensagem;
+            Destinario = destinario;
+        }
+
+        private static void ValidarEnderecoDeEmail(string endereco)
+        {
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+            if (string.IsNullOrEmpty(endereco) || !regex.Match(endereco).Success)
+                throw new FormatException("Endereço de email inválido");
+        }
+
+    }
+}
---
+++ Testes/EmailBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Dominio;
+
+namespace Testes
+{
+    public class EmailBuilder
+    {
+        private string _remetente;
+        private string _mensagem;
+        private string _destinario;
+
+        private EmailBuilder()
+        {
+            _remetente = "marlom@teste.com";
+            _destinario = "destinario@teste.com";
+            _mensagem = "Uma mensagem de email";
+        }
+
+        public static EmailBuilder Novo()
+        {
+            return new EmailBuilder();
+        }
+
+
+        public EmailBuilder ComRemetente(string remetente)
+        {
+            _remetente = remetente;
+            return this;
+        }
+
+        public EmailBuilder ComMensagem(string mensagem)
+        {
+            _mensagem = mensagem;
+            return this;
+        }
+
+        public EmailBuilder ComDestinario(string destinario)
+        {
+            _destinario = destinario;
+            return this;
+        }
+
+        public Email Criar()
+        {
+            return new Email(_remetente, _mensagem, _destinario);
+        }
+    }
+}
